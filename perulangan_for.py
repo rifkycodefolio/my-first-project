ea07060d5021f88b09d7ebbf88758210811c05f9
rifkyramdhani <rifkyramdhani@gmail.com>
2023-10-14 22:01:06 +0700

    perulangan for dan while

---
+++ perulangan_for.py
@@ -0,0 +1,6 @@
+jumlah_buku = 10
+
+jumlah_buku_dibaca = 0
+
+for jumlah_buku_dibaca in range (1, jumlah_buku+1):
+    print (f'Buku ke {jumlah_buku_dibaca} sudah dibaca')
---
+++ perulangan_while.py
@@ -0,0 +1,7 @@
+jumlah_buku = 10
+jumlah_buku_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
+while jumlah_buku_dibaca < jumlah_buku:
+    jumlah_buku_dibaca = jumlah_buku_dibaca + 1
+    print(f'Buku ke {jumlah_buku_dibaca} sudah dibaca')
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
